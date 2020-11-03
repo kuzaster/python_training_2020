d4702dd9e01953dcf5c76a6b7fb08057a85f96e3
kuzaster <as-kuz@mail@mail.ru>
2020-11-03 22:59:23 +0300

    hw1

---
+++ HW1/tasks/task02.py
@@ -0,0 +1,22 @@
+"""
+Given a cell with "it's a fib sequence" from slideshow,
+    please write function "check_fib", which accepts a Sequence of integers, and
+    returns if the given sequence is a Fibonacci sequence
+
+We guarantee, that the given sequence contain >= 0 integers inside.
+
+"""
+from typing import Sequence
+
+
+def check_fibonacci(data: Sequence[int]) -> bool:
+    if len(data) >= 3:
+        for i in range(len(data) - 2):
+            if data[i] + data[i + 1] == data[i + 2]:
+                fib = True
+            else:
+                fib = False
+                break
+        return fib
+    else:
+        return False
---
+++ HW1/tasks/task05.py
@@ -0,0 +1,23 @@
+"""
+Given a list of integers numbers "nums".
+
+You need to find a sub-array with length less equal to "k", with maximal sum.
+
+The written function should return the sum of this sub-array.
+
+Examples:
+    nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+    result = 16
+"""
+from typing import List
+
+
+def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
+    max_sum_sub = nums[0]
+    for i in range(len(nums) - k + 1):
+        cur_sum_sub = 0
+        for j in range(k):
+            cur_sum_sub += nums[i + j]
+            if cur_sum_sub > max_sum_sub:
+                max_sum_sub = cur_sum_sub
+    return max_sum_sub
