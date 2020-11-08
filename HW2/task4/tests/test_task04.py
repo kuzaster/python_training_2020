1760de423021ba4643debbf9c9f12626f619d05f
kuzaster <as-kuz@mail@mail.ru>
2020-11-09 00:51:41 +0300

    hw2.task4

---
+++ HW2/task4/tests/test_task04.py
@@ -0,0 +1,14 @@
+import pytest
+from task04 import cache
+
+
+def test_cache():
+    def func(a, b):
+        return (a ** b) ** 2
+
+    cache_func = cache(func)
+    some = 100, 200
+    val_1 = cache_func(*some)
+    val_2 = cache_func(*some)
+
+    assert val_1 is val_2
