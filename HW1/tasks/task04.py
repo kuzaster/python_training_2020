538037a8971fefcbe41324fc885e4ee3f71fef25
kuzaster <as-kuz@mail@mail.ru>
2020-11-10 10:51:10 +0300

    change loop and structure of ab and cd

--- HW1/tasks/task04.py
+++ HW1/tasks/task04.py
@@ -6,17 +6,19 @@ Given four lists A, B, C, D of integer values,
 
 We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
 """
+from collections import defaultdict
 from typing import List
 
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
-    ab, cd = {}, {}
-    for i, va in enumerate(a):
-        for j, vb in enumerate(b):
-            ab[va + vb] = 1 if (va + vb) not in ab else ab[va + vb] + 1
-            cd[c[i] + d[j]] = 1 if (c[i] + d[j]) not in cd else cd[c[i] + d[j]] + 1
+    ab, cd = defaultdict(int), defaultdict(int)
+    len_lists = len(a)
+    for i in range(len_lists):
+        for j in range(len_lists):
+            ab[a[i] + b[j]] += 1
+            cd[c[i] + d[j]] += 1
     max_four = 0
     for key, val in ab.items():
-        if -int(key) in cd:
-            max_four += max(val, cd[-int(key)])
+        if -key in cd:
+            max_four += max(val, cd[-key])
     return max_four
