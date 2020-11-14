d0c997c0d6a4deea3b7b56619d6b45a7661c2b3f
kuzaster <as-kuz@mail@mail.ru>
2020-11-15 00:02:26 +0300

    hw4.task3

--- HW4/task3/tests/test_task03.py
+++ HW4/task3/tests/test_task03.py
@@ -1,13 +1,13 @@
 from task03 import my_precious_logger
 
 
-def test_my_precious_logger_stderr(capfd):
-    my_precious_logger("error: file not found")
-    out, err = capfd.readouterr()
-    assert err == "error: file not found"
-
-
 def test_my_precious_logger_stdout(capfd):
     my_precious_logger("OK")
     out, err = capfd.readouterr()
     assert out == "OK"
+
+
+def test_my_precious_logger_stderr(capfd):
+    my_precious_logger("error: file not found")
+    out, err = capfd.readouterr()
+    assert err == "error: file not found"
