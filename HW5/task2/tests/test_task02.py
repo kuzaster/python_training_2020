b2d6ec843e67976c9856dcdc46b78cd52020c0b5
kuzaster <as-kuz@mail@mail.ru>
2020-11-20 18:36:41 +0300

    hw5.task2

---
+++ HW5/task2/tests/test_task02.py
@@ -0,0 +1,59 @@
+import pytest
+from task02 import new_wrapper, print_result
+
+
+def test_wrapped_function_not_save_internal_values():
+    def function_name(n):
+        """Some information about function"""
+        return n
+
+    function_name = print_result(function_name)
+
+    assert function_name.__name__ != "function_name"
+    assert function_name.__doc__ != "Some information about function"
+
+
+def test_wrapped_function_save_internal_values():
+    def function_name(n):
+        """Some information about function"""
+        return n
+
+    print_result = new_wrapper(
+        function_name
+    )  # apply new_wrapper to wrapping function "print_result"
+    function_name = print_result(function_name)
+
+    assert function_name.__name__ == "function_name"
+    assert function_name.__doc__ == "Some information about function"
+
+
+def test_wrapped_function_has_no_attribute_original_func():
+    def function_name(n):
+        """Some information about function"""
+        return n
+
+    function_name = print_result(function_name)
+
+    assert hasattr(function_name, "__original_func") is False
+
+
+def test_wrapped_function_has_attribute_original_func():
+    def function_name(n):
+        """Some information about function"""
+        return n
+
+    print_result = new_wrapper(function_name)
+    function_name = print_result(function_name)
+
+    assert hasattr(function_name, "__original_func") is True
+
+
+def test_wrapped_function_new_attribute_original_func_equal_this_function():
+    def function_name(n):
+        """Some information about function"""
+        return n
+
+    print_result = new_wrapper(function_name)
+    function_name = print_result(function_name)
+
+    assert function_name.__original_func == function_name
