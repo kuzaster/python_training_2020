323c5a6eaf43a7a0b8ec22bc2e9f88da3ced2ea8
kuzaster <as-kuz@mail@mail.ru>
2020-11-28 21:41:59 +0300

    hw7.task1

--- HW7/task1/task01.py
+++ HW7/task1/task01.py
@@ -8,24 +8,6 @@ Tree can only contains basic structures like:
 from itertools import chain
 from typing import Any
 
-# Example tree:
-example_tree = {
-    "first": ["RED", "BLUE"],
-    "second": {
-        "simple_key": ["simple", "list", "of", "RED", "valued"],
-    },
-    "third": {
-        "abc": "BLUE",
-        "jhl": "RED",
-        "complex_key": {
-            "key1": "value1",
-            "key2": "RED",
-            "key3": ["a", "lot", "of", "values", {"nested_key": "RED"}],
-        },
-    },
-    "fourth": "RED",
-}
-
 
 def find_occurrences(tree: dict, element: Any) -> int:
     tree = chain.from_iterable(tree.items())
@@ -44,7 +26,3 @@ def find_occurrences(tree: dict, element: Any) -> int:
 
     counter(tree, element)
     return occurs[element]
-
-
-if __name__ == "__main__":
-    print(find_occurrences(example_tree, "RED"))  # 6
