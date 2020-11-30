d3e5ed608c1211f49503166d2d977cce45217315
kuzaster <as-kuz@mail@mail.ru>
2020-11-30 16:40:19 +0300

    added better solution

--- HW4/task5/task05.py
+++ HW4/task5/task05.py
@@ -12,15 +12,22 @@ Definition of done:
 * https://en.wikipedia.org/wiki/Fizz_buzz
 ** https://www.youtube.com/watch?v=NSzsYWckGd4
 """
-from collections import defaultdict
 from typing import Generator
 
 
 def fizzbuzz(n: int) -> Generator[str, None, None]:
-    d = defaultdict(str)
-    for i in range(3, n + 1, 3):
-        d[i] = "fizz"
-    for i in range(5, n + 1, 5):
-        d[i] += "buzz"
     for i in range(1, n + 1):
-        yield d[i] or str(i)
+        first_if = not bool(i % 15) and "fizzbuzz"
+        second_if = not bool(i % 3) and "fizz"
+        third_if = not bool(i % 5) and "buzz"
+        yield first_if or second_if or third_if or i
+
+
+# def old_fizzbuzz(n: int) -> Generator[str, None, None]:
+#     d = defaultdict(str)
+#     for i in range(3, n + 1, 3):
+#         d[i] = "fizz"
+#     for i in range(5, n + 1, 5):
+#         d[i] += "buzz"
+#     for i in range(1, n + 1):
+#         yield d[i] or str(i)
