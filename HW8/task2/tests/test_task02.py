813db4101b012842f6801798f82bf76b8ce4f8a2
kuzaster <as-kuz@mail@mail.ru>
2020-12-07 14:12:46 +0300

    hw8.task2

--- HW8/task2/tests/test_task02.py
+++ HW8/task2/tests/test_task02.py
@@ -1,13 +1,27 @@
-from task02 import backspace_compare
+from task02 import TableData
+
+presidents = TableData(database_name="example.sqlite", table_name="presidents")
 
 
-def test_backspace_compare_with_equal_string():
-    assert backspace_compare("ab#c", "ad#c")
-    assert backspace_compare("a##c", "#a#c")
-    assert backspace_compare("ca#c#", "c")
+def test_method_len():
+    assert len(presidents) == 3
 
 
-def test_backspace_compare_with_unequal_string():
-    assert not backspace_compare("a#c", "#ac")
-    assert not backspace_compare("a#c", "c#")
-    assert not backspace_compare("ca#c", "c")
+def test_in_method():
+    assert "Yeltsin" in presidents
+    assert "Kim Kim" not in presidents
+
+
+def test_access_by_key():
+    assert presidents["Yeltsin"] == ("Yeltsin", 999, "Russia")
+    assert presidents["Trump"] == ("Trump", 1337, "US")
+
+
+def test_for_loops():
+    presidents_names = ["Yeltsin", "Trump", "Big Man Tyrone"]
+    presidents_ages = [999, 1337, 101]
+    index = -1
+    for president in presidents:
+        index += 1
+        assert president["name"] == presidents_names[index]
+        assert president["age"] == presidents_ages[index]
