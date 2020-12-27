8a097d959e0cbab4069d84cf4c0f0fab42b2d46e
kuzaster <as-kuz@mail@mail.ru>
2020-12-28 00:56:19 +0300

    hw12

---
+++ HW12/myproject/school/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class Homework(models.Model):
+    db_table = "Homeworks"
+
+    text = models.TextField()
+    deadline = models.DurationField()
+    created = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.text
+
+
+class HomeworkResult(models.Model):
+    db_table = "HomeworkResults"
+
+    homework = models.ForeignKey(Homework, on_delete=models.CASCADE)
+    author = models.ForeignKey("Student", on_delete=models.CASCADE)
+    solution = models.TextField()
+    created = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.solution
+
+
+class PersonalData(models.Model):
+    class Meta:
+        abstract = True
+
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Student(PersonalData):
+    db_table = "Students"
+    age = models.IntegerField()
+
+
+class Teacher(PersonalData):
+    db_table = "Teachers"
+    homework_done = models.ForeignKey(HomeworkResult, on_delete=models.CASCADE)
