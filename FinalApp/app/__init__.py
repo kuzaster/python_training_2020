c4ac6d713fab0ad8cf692b6ad84b9e06f1dd5e4a
Artem <as-kuz@mail.ru>
2021-01-21 14:33:25 +0300

    final project

--- FinalApp/app/__init__.py
+++ FinalApp/app/__init__.py
@@ -1,6 +1,7 @@
-from app import routes
 from config import Config
 from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+from app import routes
--- FinalApp/app/containers.py
+++ FinalApp/app/containers.py
@@ -1,12 +1,9 @@
-import json
 import os
 import time
 
 import docker
 import yaml
 
-# with open(os.path.join(os.path.dirname(__file__), "config-file.json")) as conf:
-#     copy_config_file = json.load(conf)
 config_path = os.path.join(os.path.dirname(__file__), "config-file.yaml")
 copy_path = os.path.join(os.path.dirname(__file__), "copy-config-file.yaml")
 
@@ -22,15 +19,29 @@ def get_cont_by_name(name):
             return cont
 
 
+def update_timestamp(new_stamp):
+    with open(os.path.join(os.path.dirname(__file__), "timestamp.txt"), "w") as stamp:
+        stamp.write(str(new_stamp))
+
+
+def update_config(new_data, path=copy_path):
+    with open(path, "w") as conf:
+        yaml.dump(new_data, conf)
+
+
 def start_containers():
     client = docker.from_env()
-    imgs = client.images
     conts = client.containers
-    for container in get_all_containers():
+    containers = get_all_containers()
+    for container in containers:
         options, dock_path = container["options"], container["docker_path"]
-        name = options["name"]
+        name = None if "name" not in options else options["name"]
         if not conts.list(all=True, filters={"name": name}):
-            run_container(options, dock_path)
+            run_cont = run_container(options, dock_path)
+            if not name:
+                container["options"]["name"] = run_cont.name
+                update_config(containers, config_path)
+                update_config(containers)
         elif conts.get(name).status != "running":
             conts.get(name).start()
             print(f"Container {name} started!")
@@ -46,12 +57,13 @@ def change_container(old_name, new_opts, new_path, new_port, new_url):
             container = client.containers.get(old_name)
             container.stop()
             container.remove()
-            run_container(new_opts, new_path)
+            run_cont = run_container(new_opts, new_path)
             cont["options"], cont["docker_path"] = new_opts, new_path
             cont["port"], cont["public_url"] = new_port, new_url
-            update_copy(containers)
-            with open(config_path, "w") as conf:
-                yaml.dump(containers, conf)
+            if "name" not in cont["options"]:
+                cont["options"]["name"] = run_cont.name
+            update_config(containers, config_path)
+            update_config(containers)
             update_timestamp(os.stat(config_path).st_mtime)
             break
 
@@ -60,14 +72,12 @@ def run_container(options=None, dock_path=None):
     client = docker.from_env()
     imgs = client.images
     conts = client.containers
-    # if conf_cont:
-    #     options, dock_path, port, pub_url = conf_cont.values()
-    # name = int(pub_url[17:])
     # build image
     cont_image = imgs.build(path=dock_path)[0]
     # create container
-    conts.run(image=cont_image, **options)
-    print(f"Container {options['name']} run!")
+    container = conts.run(image=cont_image, **options)
+    print(f"Container {container.name} run!")
+    return container
 
 
 def add_container(options, dock_path, port, pub_url):
@@ -78,12 +88,13 @@ def add_container(options, dock_path, port, pub_url):
         "public_url": pub_url,
     }
 
+    run_cont = run_container(options, dock_path)
+    if "name" not in cont_config["options"]:
+        cont_config["options"]["name"] = run_cont.name
     containers = get_all_containers()
     containers.append(cont_config)
-    with open(config_path, "w") as conf:
-        yaml.dump(containers, conf)
-    run_container(options, dock_path)
-    update_copy(containers)
+    update_config(containers, config_path)
+    update_config(containers)
     update_timestamp(os.stat(config_path).st_mtime)
 
 
@@ -94,21 +105,10 @@ def remove_container(name):
     container.remove()
     containers = get_all_containers()
     containers.remove(get_cont_by_name(name))
-    with open(config_path, "w") as conf:
-        yaml.dump(containers, conf)
+    update_config(containers, config_path)
     print(f"Container {name} stopped and removed")
-    update_copy(containers)
+    update_config(containers)
     update_timestamp(os.stat(config_path).st_mtime)
-
-
-def update_timestamp(new_stamp):
-    with open(os.path.join(os.path.dirname(__file__), "timestamp.txt"), "w") as stamp:
-        stamp.write(str(new_stamp))
-
-
-def update_copy(new_data):
-    with open(copy_path, "w") as conf:
-        yaml.dump(new_data, conf)
 
 
 def find_changed_cont_and_update():
@@ -127,13 +127,15 @@ def find_changed_cont_and_update():
                     changed_conts[ind]["options"],
                     changed_conts[ind]["docker_path"],
                 )
-                run_container(new_opts, new_path)
+                run_cont = run_container(new_opts, new_path)
+                if "name" not in changed_conts[ind]["options"]:
+                    changed_conts[ind]["options"]["name"] = run_cont.name
                 print(f"Container {cont['options']['name']} changed and re-run!")
-                update_copy(changed_conts)
+                update_config(changed_conts, config_path)
+                update_config(changed_conts)
                 update_timestamp(config_timestamp)
     elif len(changed_conts) > len(old_conts):
         start_containers()
-        update_copy(changed_conts)
         update_timestamp(config_timestamp)
     else:
         for cont in old_conts:
@@ -141,7 +143,7 @@ def find_changed_cont_and_update():
                 container = conts.get(cont["options"]["name"])
                 container.stop()
                 container.remove()
-                update_copy(changed_conts)
+                update_config(changed_conts)
                 update_timestamp(config_timestamp)
 
 
@@ -165,40 +167,3 @@ def watch_file_update():
         else:
             with open(time_path, "w") as stamp:
                 stamp.write(str(os.stat(config_path).st_mtime))
-
-
-# path = os.path.join(os.path.dirname(__file__), "copy-config-file.json")
-# print(get_all_containers())
-# print(get_all_containers(path))
-# timestamp = os.stat(path).st_mtime
-# while True:
-#     if copy_config_file != get_all_containers():
-#         print("They differents")
-# watch_file_update()
-"""
-config = {
-        "docker_path": "/home/tim/TestsProjects/Flask/DemoDocker/app_1",
-        "options": {
-                    "ports": {7000: 7777},
-                    "detach": True
-                    },
-        "port": 7000,
-        "public_url": "http://localhost:7777"
-    }
-options = {
-        "name": "timmy-test-server",
-        "ports": {7000: 7777},
-        "detach": True
-}
-client = docker.from_env()
-imgs = client.images
-conts = client.containers
-name, dock_path = options["name"], config["docker_path"]
-# build image
-cont_image = imgs.build(path=dock_path)[0]
-# create container
-conf_options = config["options"]
-container = conts.run(image=cont_image, **conf_options)
-print(f"Container {name} run!")
-print(container.name)
-"""
